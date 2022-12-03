d5ea90882ce8429bb35e26fd34b0eafa53f01a64
Tobias Meggendorfer <tobias@meggendorfer.de>
2022-12-03 09:02:15 +0100

    draft of evaluation

---
+++ pyjudge/evaluate/render.py
@@ -0,0 +1,300 @@
+import datetime
+import logging
+import math
+import pathlib
+from typing import List
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pyjudge.evaluate.statistics import ProblemStatistics, SummaryStatistics
+from pyjudge.model import Verdict
+
+verdict_formats = [
+    ({Verdict.CORRECT}, {"label": "AC", "color": "green"}),
+    ({Verdict.WRONG_ANSWER}, {"label": "WA", "color": "red"}),
+    ({Verdict.RUN_ERROR}, {"label": "RTE", "color": "orange"}),
+    ({Verdict.TIME_LIMIT}, {"label": "TLE", "color": "purple"}),
+]
+fallback_format = {"label": "Other", "color": "gray"}
+
+
+class ProblemRenderer(object):
+    def __init__(self, start_time: datetime.datetime, end_time: datetime.datetime):
+        self.start_time = start_time
+        self.end_time = end_time
+
+        start_day = start_time.replace(hour=0, minute=0, second=0, microsecond=0)
+        delta = datetime.timedelta(days=1.0)
+
+        while start_day < start_time:
+            start_day += delta
+        self.start_day = start_day
+
+        days = []
+        while start_day <= end_time:
+            days.append(start_day)
+            start_day += delta
+
+        if len(days) > 7:
+            days = days[:: len(days) // 7]
+        self.day_seconds = days
+
+        self.start_second = int(start_time.timestamp())
+        self.end_second = int(end_time.timestamp())
+        self.day_beginning_seconds = [
+            int(day.timestamp()) - self.start_second for day in days
+        ]
+        self.day_names = [day.strftime("%d.%m.%y") for day in days]
+
+        self.seconds_per_bucket = (
+            3600 * 6 * (2 ** (int(math.ceil(math.log(len(days)) / math.log(7)))))
+        )
+        self.submission_buckets = (
+            self.end_second - self.start_second
+        ) // self.seconds_per_bucket + 1
+
+    def render_problem_submission_statistics(
+        self, problem_stats: ProblemStatistics, file: pathlib.Path
+    ):
+        x_axis = np.arange(start=0, stop=self.submission_buckets)
+
+        verdict_data = {verdict: np.zeros(len(x_axis)) for verdict in Verdict}
+        for (
+            verdict,
+            timestamps,
+        ) in problem_stats.overall.verdicts_by_time.items():
+            for timestamp in timestamps:
+                x_pos = (int(timestamp) - self.start_second) // self.seconds_per_bucket
+                if not 0 <= x_pos < self.submission_buckets:
+                    logging.warning(
+                        "Invalid submission time %f (should be between %s and %s)",
+                        timestamp,
+                        self.start_time,
+                        self.end_time,
+                    )
+                    continue
+                verdict_data[verdict][x_pos] += 1
+
+        fig, ax = plt.subplots()
+
+        graph_data = []
+        handled_verdicts = set()
+        for verdicts, verdict_format in verdict_formats:
+            handled_verdicts.update(verdicts)
+            values = sum(verdict_data[verdict] for verdict in verdicts)
+            if values.any():
+                graph_data.append(
+                    (values, verdict_format["label"], verdict_format["color"])
+                )
+        if len(Verdict) > len(handled_verdicts):
+            other_verdict_values = sum(
+                verdict_data[verdict]
+                for verdict in Verdict
+                if verdict not in handled_verdicts
+            )
+            if other_verdict_values.any():
+                graph_data.append(
+                    (
+                        other_verdict_values,
+                        fallback_format["label"],
+                        fallback_format["color"],
+                    )
+                )
+
+        summed = np.zeros(len(x_axis))
+        for value, label, color in graph_data:
+            ax.bar(
+                x_axis * self.seconds_per_bucket + self.seconds_per_bucket // 2,
+                value,
+                width=int(math.ceil(self.seconds_per_bucket * 0.8)),
+                label=label,
+                color=color,
+                bottom=summed,
+            )
+            summed += value
+        ax.set_xlim([-1, self.submission_buckets * self.seconds_per_bucket])
+        ax.legend()
+        ax.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
+        plt.xticks(ticks=self.day_seconds, labels=self.day_names)
+        with file.open(mode="wb") as f:
+            plt.savefig(f, format="png", transparent=True)
+
+
+class SummaryRenderer(object):
+    def __init__(self, data: SummaryStatistics):
+        self.data = data
+
+    def render_per_day(self, path: pathlib.Path, day_labels: List[str]):
+        fig, ax = plt.subplots()
+        per_day_x_axis = np.arange(len(day_labels))
+        per_day_width = 0.25
+
+        ax.bar(
+            per_day_x_axis,
+            self.data.submissions_per_day,
+            width=per_day_width,
+            color="orange",
+            label="Submissions",
+        )
+        ax.bar(
+            per_day_x_axis + per_day_width,
+            self.data.clarifications_per_day,
+            width=per_day_width,
+            color="teal",
+            label="Clarifications",
+        )
+
+        ax.legend()
+        ax.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
+        ax.set_xticks(per_day_x_axis + per_day_width / 2)
+        ax.set_xticklabels(day_labels)
+
+        with path.open(mode="wb") as f:
+            plt.savefig(f, format="png", transparent=True)
+
+    def render_per_hour(self, path: pathlib.Path):
+        fig, ax = plt.subplots()
+        per_hour_x_axis = np.arange(24)
+        per_hour_width = 0.25
+
+        ax.bar(
+            per_hour_x_axis,
+            self.data.submissions_per_hour,
+            width=per_hour_width,
+            color="orange",
+            label="Submissions",
+        )
+        ax.bar(
+            per_hour_x_axis + per_hour_width,
+            self.data.clarifications_per_hour,
+            width=per_hour_width,
+            color="teal",
+            label="Clarifications",
+        )
+        ax.legend()
+        ax.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
+        ax.set_xticks(per_hour_x_axis + per_hour_width / 2)
+        ax.set_xticklabels(per_hour_x_axis)
+
+        with path.open(mode="wb") as f:
+            plt.savefig(f, format="png", transparent=True)
+
+    def render_per_day_hour(self, path: pathlib.Path, day_labels: List[str]):
+        fig, ax = plt.subplots(figsize=(9, 3))
+        im = ax.imshow(self.data.submissions_per_day_hour)
+        ax.set_xticks(np.arange(24))
+        ax.set_yticks(np.arange(len(day_labels)))
+        ax.set_yticklabels(day_labels)
+        ax.spines[:].set_visible(False)
+
+        formatter = matplotlib.ticker.StrMethodFormatter("{x:d}")
+        for day, hour_submissions in enumerate(self.data.submissions_per_day_hour):
+            for hour, count in enumerate(hour_submissions):
+                im.axes.text(
+                    hour,
+                    day,
+                    formatter(count),
+                    horizontalalignment="center",
+                    verticalalignment="center",
+                    color="white",
+                )
+
+        cbar = ax.figure.colorbar(im, ax=ax)
+        cbar.ax.set_ylabel("Submissions", rotation=-90, va="bottom")
+        fig.tight_layout()
+
+        with path.open(mode="wb") as f:
+            plt.savefig(f, format="png", transparent=True)
+
+    def render_per_remaining_time(self, path: pathlib.Path):
+        fig, ax = plt.subplots()
+        ax.bar(
+            range(len(self.data.submissions_by_time_remaining)),
+            self.data.submissions_by_time_remaining,
+            width=0.4,
+            color="orange",
+            label="Submissions",
+        )
+        ax.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
+        ax.set_xticks(np.arange(len(self.data.submissions_by_time_remaining)))
+        ax.set_xticklabels([f"{i:d}%" for i in range(100, 0, -10)])
+
+        with path.open(mode="wb") as f:
+            plt.savefig(f, format="png", transparent=True)
+
+    def render_per_contest(self, path: pathlib.Path):
+        fig, ax = plt.subplots()
+        per_contest_x_axis = np.arange(len(self.data.contest_keys))
+        per_contest_width = 0.25
+
+        ax.bar(
+            per_contest_x_axis,
+            [
+                self.data.submissions_by_contest[contest_key]
+                for contest_key in self.data.contest_keys
+            ],
+            width=per_contest_width,
+            color="orange",
+            label="Submissions",
+        )
+        ax.bar(
+            per_contest_x_axis + per_contest_width,
+            [
+                self.data.clarifications_by_contest[contest_key]
+                for contest_key in self.data.contest_keys
+            ],
+            width=per_contest_width,
+            color="teal",
+            label="Clarifications",
+        )
+        ax.legend()
+        ax.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
+        ax.set_xticks(per_contest_x_axis + per_contest_width / 2)
+        ax.set_xticklabels(self.data.contest_keys, rotation=-30)
+
+        with path.open(mode="wb") as f:
+            plt.savefig(f, format="png", transparent=True)
+
+    def render_per_language(self, path: pathlib.Path):
+        fig, ax = plt.subplots()
+        submissions_by_language_x_axis = np.arange(
+            len(self.data.submissions_by_language.keys())
+        )
+        submissions_by_language_width = 0.25
+        per_contest_width = 0.25
+
+        language_keys = sorted(list(self.data.submissions_by_language.keys()))
+        language_submissions_correct = []
+        language_submissions_incorrect = []
+        for language_key in language_keys:
+            correct_submissions = self.data.submissions_by_language_correct[
+                language_key
+            ]
+            language_submissions_correct.append(correct_submissions)
+            language_submissions_incorrect.append(
+                self.data.submissions_by_language[language_key] - correct_submissions
+            )
+
+        ax.bar(
+            submissions_by_language_x_axis,
+            language_submissions_correct,
+            width=submissions_by_language_width,
+            color="green",
+            label="Correct",
+        )
+        ax.bar(
+            submissions_by_language_x_axis + submissions_by_language_width,
+            language_submissions_incorrect,
+            width=submissions_by_language_width,
+            color="red",
+            label="Wrong",
+        )  # bottom=submissions_by_language_correct
+        ax.legend()
+        ax.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
+        ax.set_xticks(submissions_by_language_x_axis + per_contest_width / 2)
+        ax.set_xticklabels(language_keys)
+
+        with path.open(mode="wb") as f:
+            plt.savefig(f, format="png", transparent=True)
---
+++ pyjudge/evaluate/statistics.py
@@ -0,0 +1,406 @@
+import dataclasses
+import itertools
+import math
+import pathlib
+import statistics
+import datetime
+from collections import defaultdict
+from typing import Set, Dict, List
+
+import matplotlib
+import numpy as np
+import pytz
+from matplotlib import pyplot as plt
+
+from pyjudge.data.submission import SubmissionDto, ContestDataDto
+from pyjudge.model import Verdict
+
+
+@dataclasses.dataclass
+class ProblemGroupStatistics(object):
+    submission_count: int
+    correct_submission_count: int
+    teams_with_attempt: Set[str]
+    teams_with_solution: Set[str]
+    submissions_until_correct_by_team: Dict[str, int]
+
+    first_teams: List[str]
+    fastest_teams: List[str]
+    shortest_teams: List[str]
+    smallest_teams: List[str]
+
+    verdict_count: Dict[Verdict, int]
+    verdicts_by_time: Dict[Verdict, List[float]]
+
+    @staticmethod
+    def of(submissions: List[SubmissionDto]):
+        correct_submissions = [
+            submission
+            for submission in submissions
+            if submission.verdict == Verdict.CORRECT
+        ]
+        submission_count: int = len(submissions)
+        correct_submission_count: int = len(correct_submissions)
+        teams_with_attempt: Set[str] = set(
+            submission.team_key for submission in submissions
+        )
+        teams_with_solution: Set[str] = set(
+            submission.team_key for submission in correct_submissions
+        )
+
+        submissions_by_team = defaultdict(list)
+        for submission in submissions:
+            submissions_by_team[submission.team_key].append(submission)
+
+        submissions_by_team_sorted = {
+            team_key: sorted(team_submissions, key=lambda s: s.submission_time)
+            for team_key, team_submissions in submissions_by_team.items()
+        }
+
+        correct_submissions_by_team = dict()
+        for team_key, team_submissions in submissions_by_team_sorted.items():
+            correct_submissions = [
+                submission
+                for submission in team_submissions
+                if submission.verdict == Verdict.CORRECT
+            ]
+            if correct_submissions:
+                correct_submissions_by_team[team_key] = correct_submissions
+
+        first_correct_submission_by_team: Dict[str, SubmissionDto] = {
+            team_key: min(team_submissions, key=lambda s: s.submission_time)
+            for team_key, team_submissions in correct_submissions_by_team.items()
+        }
+        submissions_until_correct_by_team = {
+            team_key: len(
+                [
+                    submission
+                    for submission in submissions_by_team_sorted[team_key]
+                    if submission.submission_time
+                    <= team_correct_submission.submission_time
+                ]
+            )
+            for team_key, team_correct_submission in first_correct_submission_by_team.items()
+        }
+
+        first_teams = sorted(
+            list(first_correct_submission_by_team.items()),
+            key=lambda item: item[1].submission_time,
+        )
+        fastest_correct_submission_by_team: Dict[str, SubmissionDto] = {
+            team_key: min(team_submissions, key=lambda s: s.maximum_runtime)
+            for team_key, team_submissions in correct_submissions_by_team.items()
+        }
+        fastest_teams = sorted(
+            list(fastest_correct_submission_by_team.items()),
+            key=lambda item: item[1].maximum_runtime,
+        )
+        shortest_correct_submission_by_team: Dict[str, SubmissionDto] = {
+            team_key: min(team_submissions, key=lambda s: s.line_count)
+            for team_key, team_submissions in correct_submissions_by_team.items()
+        }
+        shortest_teams = sorted(
+            list(shortest_correct_submission_by_team.items()),
+            key=lambda item: item[1].line_count,
+        )
+        smallest_correct_submission_by_team: Dict[str, SubmissionDto] = {
+            team_key: min(team_submissions, key=lambda s: s.byte_size)
+            for team_key, team_submissions in correct_submissions_by_team.items()
+        }
+        smallest_teams = sorted(
+            list(smallest_correct_submission_by_team.items()),
+            key=lambda item: item[1].byte_size,
+        )
+
+        verdict_count: Dict[Verdict, int] = defaultdict(lambda: 0)
+        verdicts_by_time: Dict[Verdict, List[float]] = defaultdict(list)
+        for submission in submissions:
+            verdicts_by_time[submission.verdict].append(submission.submission_time)
+
+        return ProblemGroupStatistics(
+            submission_count=submission_count,
+            correct_submission_count=correct_submission_count,
+            teams_with_attempt=teams_with_attempt,
+            teams_with_solution=teams_with_solution,
+            submissions_until_correct_by_team=submissions_until_correct_by_team,
+            first_teams=first_teams,
+            fastest_teams=fastest_teams,
+            shortest_teams=shortest_teams,
+            smallest_teams=smallest_teams,
+            verdict_count=verdict_count,
+            verdicts_by_time=verdicts_by_time,
+        )
+
+
+@dataclasses.dataclass
+class ProblemStatistics(object):
+    by_language: Dict[str, ProblemGroupStatistics]
+    overall: ProblemGroupStatistics
+
+    @staticmethod
+    def of(submissions: List[SubmissionDto]):
+        submissions_by_language = defaultdict(list)
+        for submission in submissions:
+            submissions_by_language[submission.language_key].append(submission)
+
+        by_language = {
+            language_key: ProblemGroupStatistics(language_submissions)
+            for language_key, language_submissions in submissions_by_language.items()
+        }
+        overall = ProblemGroupStatistics(submissions)
+        return ProblemStatistics(by_language=by_language, overall=overall)
+
+
+@dataclasses.dataclass
+class ContestStatistics(object):
+    team_name_by_key: Dict[str, str]
+    language_name_by_key: Dict[str, str]
+    problem_statistics_by_key: Dict[str, ProblemStatistics]
+
+    @staticmethod
+    def of(contest_data: ContestDataDto):
+        team_name_by_key = {
+            team.key: team.display_name for team in contest_data.teams.values()
+        }
+        language_name_by_key = contest_data.languages
+
+        submissions = [
+            submission
+            for submission in contest_data.submissions
+            if not submission.too_late
+        ]
+        submissions_by_problem = defaultdict(list)
+        for submission in submissions:
+            problem = contest_data.problems[submission.contest_problem_key]
+            submissions_by_problem[problem].append(submission)
+
+        problem_statistics_by_key: Dict[str, ProblemStatistics] = {
+            problem_key: ProblemStatistics.of(submissions)
+            for problem_key, submissions in submissions_by_problem.items()
+        }
+        return ContestStatistics(
+            team_name_by_key=team_name_by_key,
+            language_name_by_key=language_name_by_key,
+            problem_statistics_by_key=problem_statistics_by_key,
+        )
+
+
+@dataclasses.dataclass
+class SummaryStatistics(object):
+    contest_keys: List[str]
+
+    total_active_users: int
+    total_contest_count: int
+    total_problem_count: int
+    total_solved_problem_count: int
+    total_submission_count: int
+    total_lines_of_code: int
+    total_clarification_count: int
+
+    average_clarification_time: float
+    median_clarification_time: float
+    average_submissions_per_contest: float
+    average_clarifications_per_contest: float
+
+    minimum_solution_line_count: int
+
+    submissions_per_day: List[int]
+    submissions_per_hour: List[int]
+    submissions_per_day_hour: List[List[int]]
+    submissions_by_time_remaining: List[int]
+    submissions_by_contest: Dict[str, int]
+    submissions_by_problem: Dict[str, int]
+    submissions_by_language: Dict[str, int]
+    submissions_by_language_correct: Dict[str, int]
+
+    clarifications_per_day: List[int]
+    clarifications_per_hour: List[int]
+    clarifications_by_contest: Dict[str, int]
+    clarification_response_times: List[int]
+    clarifications_by_key: Dict[str, int]
+    clarifications_by_problem: Dict[str, int]
+
+    @staticmethod
+    def of(
+        contests_data: List[ContestDataDto],
+        timezone: pytz.BaseTzInfo,
+        submission_filter=None,
+    ) -> "SummaryStatistics":
+        contests_data.sort(key=lambda contest_data: contest_data.description.start)
+        contest_data_by_key = {
+            contest_data.description.contest_key: contest_data
+            for contest_data in contests_data
+        }
+
+        all_clarifications = list(
+            itertools.chain(
+                *[contest_data.clarifications for contest_data in contests_data]
+            )
+        )
+        all_submissions = list(
+            itertools.chain(
+                *[contest_data.submissions for contest_data in contests_data]
+            )
+        )
+
+        contest_keys = [
+            contest_data.description.contest_key for contest_data in contests_data
+        ]
+        solved_problems = set()
+        active_users = set()
+        smallest_solution_by_problem = dict()
+
+        submissions_per_day = [0] * 7
+        submissions_per_hour = [0] * 24
+        submissions_per_day_hour = [[0] * 24 for _ in range(7)]
+        submissions_per_contest = defaultdict(lambda: 0)
+        submissions_by_problem = defaultdict(lambda: 0)
+        submissions_by_language = defaultdict(lambda: 0)
+        submissions_by_language_correct = defaultdict(lambda: 0)
+        submissions_by_time_remaining = [0] * 10
+
+        for submission in all_submissions:
+            if submission.too_late:
+                continue
+            if not submission.is_source_submission:
+                continue
+            submission_team = contest_data_by_key[submission.contest_key].teams[
+                submission.team_key
+            ]
+            if submission_team is None:
+                continue
+            if submission_filter is not None and not submission_filter(
+                submission, submission_team
+            ):
+                continue
+
+            problem_unique_key = (
+                submission.contest_key,
+                submission.contest_problem_key,
+            )
+            submission_time = datetime.datetime.fromtimestamp(
+                submission.submission_time, timezone
+            )
+            submissions_per_day[submission_time.weekday()] += 1
+            submissions_per_hour[submission_time.hour] += 1
+            submissions_per_day_hour[submission_time.weekday()][
+                submission_time.hour
+            ] += 1
+            submissions_per_contest[submission.contest_key] += 1
+            submissions_by_problem[problem_unique_key] += 1
+            submissions_by_language[submission.language_key] += 1
+            active_users.update(
+                set(user.login_name for user in submission_team.members)
+            )
+
+            contest_description = contest_data_by_key[
+                submission.contest_key
+            ].description
+            contest_start = contest_description.start
+            contest_end = contest_description.end
+            submissions_by_time_remaining[
+                int(
+                    (
+                        (contest_end - submission.submission_time)
+                        / (contest_end - contest_start)
+                    )
+                    * 10
+                )
+            ] += 1
+
+            if submission.verdict == Verdict.CORRECT:
+                submissions_by_language_correct[submission.language_key] += 1
+                solved_problems.add(problem_unique_key)
+                smallest_solution_by_problem[problem_unique_key] = min(
+                    smallest_solution_by_problem.get(problem_unique_key, math.inf),
+                    submission.line_count,
+                )
+
+        clarifications_per_day = [0] * 7
+        clarifications_per_hour = [0] * 24
+        clarifications_per_contest = defaultdict(lambda: 0)
+        clarification_response_times = []
+        clarifications_by_key = {
+            clarification.key: clarification for clarification in all_clarifications
+        }
+        clarifications_by_problem = defaultdict(lambda: 0)
+
+        clarification_responses = {
+            clarifications_by_key[clarification.response_to].key: clarification.key
+            for clarification in all_clarifications
+            if clarification.response_to is not None
+        }
+
+        for clarification in all_clarifications:
+            if (
+                clarification.from_jury
+                or clarification.key not in clarification_responses
+            ):
+                continue
+            contest_data = contest_data_by_key[clarification.contest_key]
+            description = contest_data.description
+            if description.start <= clarification.request_time <= description.end:
+                response = clarifications_by_key[
+                    clarification_responses[clarification.key]
+                ]
+                if description.start <= response.request_time <= description.end:
+                    clarification_time = datetime.datetime.fromtimestamp(
+                        clarification.request_time, timezone
+                    )
+                    clarifications_per_day[clarification_time.weekday()] += 1
+                    clarifications_per_hour[clarification_time.hour] += 1
+                    clarifications_per_contest[clarification.contest_key] += 1
+
+                    clarification_response_times.append(
+                        response.request_time - clarification.request_time
+                    )
+                    clarifications_by_problem[
+                        (clarification.contest_key, clarification.contest_problem_key)
+                    ] += 1
+
+        average_clarification_time = statistics.mean(clarification_response_times) / 60
+        median_clarification_time = statistics.median(clarification_response_times) / 60
+        average_submissions_per_contest = statistics.mean(
+            submissions_per_contest.values()
+        )
+        average_clarifications_per_contest = statistics.mean(
+            clarifications_per_contest.values()
+        )
+
+        return SummaryStatistics(
+            contest_keys=contest_keys,
+            total_active_users=len(active_users),
+            total_contest_count=len(contest_keys),
+            total_problem_count=sum(
+                len(contest_data.problems) for contest_data in contests_data
+            ),
+            total_solved_problem_count=len(solved_problems),
+            total_submission_count=sum(
+                len(contest_data.submissions) for contest_data in contests_data
+            ),
+            total_lines_of_code=sum(
+                submission.line_count for submission in all_submissions
+            ),
+            total_clarification_count=sum(
+                len(contest_data.clarifications) for contest_data in contests_data
+            ),
+            average_clarification_time=average_clarification_time,
+            median_clarification_time=median_clarification_time,
+            average_submissions_per_contest=average_submissions_per_contest,
+            average_clarifications_per_contest=average_clarifications_per_contest,
+            minimum_solution_line_count=sum(smallest_solution_by_problem.values()),
+            submissions_per_day=submissions_per_day,
+            submissions_per_hour=submissions_per_hour,
+            submissions_per_day_hour=submissions_per_day_hour,
+            submissions_by_time_remaining=submissions_by_time_remaining,
+            submissions_by_contest=submissions_per_contest,
+            submissions_by_problem=submissions_by_problem,
+            submissions_by_language=submissions_by_language,
+            submissions_by_language_correct=submissions_by_language_correct,
+            clarifications_by_key=clarifications_by_key,
+            clarifications_per_day=clarifications_per_day,
+            clarifications_per_hour=clarifications_per_hour,
+            clarifications_by_contest=clarifications_per_contest,
+            clarification_response_times=clarification_response_times,
+            clarifications_by_problem=clarifications_by_problem,
+        )
